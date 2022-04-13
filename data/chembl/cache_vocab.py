578b0e8a8f886dff2e0c8654b220d1d07ef1ceb6
alstonlo <40709307+alstonlo@users.noreply.github.com>
2022-04-13 03:54:37 -0400

    debug vocab

--- data/chembl/cache_vocab.py
+++ data/chembl/cache_vocab.py
@@ -8,10 +8,12 @@ from src.data.vocab import FragmentVocab
 
 if __name__ == "__main__":
     # script to cache extracted vocab from ChEMBL
+    debug = True  # TODO: set to False
 
     def mol_iter(path):
         with open(path, "r") as f:
             lines = f.readlines()
+            lines = lines[:10000] if debug else lines
         for line in tqdm(lines, desc="Extracting fragments"):
             smiles = line.strip()
             mol = Chem.MolFromSmiles(smiles)
