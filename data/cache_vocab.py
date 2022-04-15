82bb9ebd68d4ee2d8cb4c4d001a257352b0ed4ba
alstonlo <40709307+alstonlo@users.noreply.github.com>
2022-04-15 18:24:56 -0400

    Update cache_vocab.py

--- data/cache_vocab.py
+++ data/cache_vocab.py
@@ -8,7 +8,7 @@ from src.data.vocab import FragmentVocab
 
 if __name__ == "__main__":
     # script to cache extracted vocab from ChEMBL
-    debug = False  # TODO: set to False
+    debug = False
 
     def mol_iter(path):
         with open(path, "r") as f:
